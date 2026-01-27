79ebc672bae728f15726ab9aa8beee4a303ce7cf
skumar <141301430+webskumar@users.noreply.github.com>
2026-01-27 07:33:25 +0000

    1question

---
+++ IF–ELSE-Practice/01_Qustion.py
@@ -0,0 +1,9 @@
+#Q1 Take a number as input and check whether it is Positive or Negative.
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero (neither positive nor negative)")
