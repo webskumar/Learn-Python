fda4c0826c5c8aa34c0596a12e35990aed90f6f7
skumar <141301430+webskumar@users.noreply.github.com>
2026-02-02 09:33:03 +0000

    Loop reverse

--- 02_Loop/05_Practise.py
+++ 02_Loop/05_Practise.py
@@ -1,2 +1,8 @@
 #5. Intermediate Challenge
 #Question: Write a program to reverse a number using a loop (e.g., 1234 becomes 4321).
+num = 1234
+reverse = 0
+while num > 0:
+    reverse = (reverse * 10) + (num % 10)
+    num //= 10
+print(reverse)
