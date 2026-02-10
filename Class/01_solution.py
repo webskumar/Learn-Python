4878e288a0315bb83e094a3b4b20c5134681eace
skumar <141301430+webskumar@users.noreply.github.com>
2026-02-10 17:00:41 +0000

    class

---
+++ Class/01_solution.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+s1 = Person("Ram Kumar", 21)
+s2 = Person("Mohan Kumar", 24)
+
+print(s1.name, s1.age)
+print(s2.name, s2.age)
